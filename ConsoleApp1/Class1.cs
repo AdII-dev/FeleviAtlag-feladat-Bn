76b96abdb6dd45d3f1ce5639a6203d556c933f39
Adrián Vizi <adrian.vizi04@gmail.com>
2023-11-13 10:20:06 +0100

    Add project files.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Adat
+    {
+        string TanuloNeve { get; set; }
+        int OktatasiAzonosito { get; set; }
+        int Halozatok1 { get; set; }
+        int Halozatok2 { get; set; }
+        int Programozas { get; set; }
+        int ProgramozasGyakorlat {  get; set; }
+        int AngolNyelv { get; set; }
+        int MagyarNyelvÉsIrodalom { get; set; }
+        int Matematika { get; set; }
+        int Tortenelem { get; set; }
+
+        public Adat(string sor) {
+        var m = sor.Split(' ');
+           
+            TanuloNeve = m[0];
+            OktatasiAzonosito = Convert.ToInt32(m[1]);
+            Halozatok1 = Convert.ToInt32(m[2]);
+            Halozatok2 = Convert.ToInt32(m[3]);
+            Programozas = Convert.ToInt32(m[4]);
+            ProgramozasGyakorlat = Convert.ToInt32(m[5]);
+            AngolNyelv = Convert.ToInt32(m[6]);
+            MagyarNyelvÉsIrodalom = Convert.ToInt32(m[7]);
+            Matematika = Convert.ToInt32(m[8]);
+            Tortenelem = Convert.ToInt32(m[9]);
+            
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+
+
+
+List<Adat> adatok = new();
+using StreamReader sr = new(
+    path: @"..\..\..\SRC\adatok.txt",
+    encoding: System.Text.Encoding.UTF8);
+while (!sr.EndOfStream) adatok.Add(new Adat(sr.ReadLine()));
+
+Console.WriteLine(adatok.Count());
+
+
